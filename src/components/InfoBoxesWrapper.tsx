44c15e98388b66b9bdbc990bc8a542176f00c814
CEJSH <eun2763@naver.com>
2024-08-01 17:46:05 +0900

    feature: Fix css

--- src/components/InfoBoxesWrapper.tsx
+++ src/components/InfoBoxesWrapper.tsx
@@ -1,7 +1,7 @@
 export default function InfoBoxesWrapper({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex pt-[40px] pb-[28px] z-10 text-black bg-white h-full">
-      <div className="flex flex-row gap-[30px] md:mx-[22%] mx-0 w-full">{children}</div>
+      <div className="flex flex-row gap-[30px] md:mx-[22%] mx-[16px] w-full">{children}</div>
     </div>
   );
 }
--- src/components/InputWrapper.tsx
+++ src/components/InputWrapper.tsx
@@ -14,12 +14,12 @@ export function InputWrapper({ children }: { children: React.ReactNode }) {
         className="absolute overflow-hidden object-cover"
       />
       {/**상단 Upper narrow part */}
-      <div className="z-10 h-full w-full max-w-[1050px] flex flex-row  justify-center">
+      <div className="z-10 px-[16px] h-full w-full max-w-[1050px] flex flex-row  justify-center">
         <div className="pt-[156px]">
-          <div className="font-[700] h-[46px] text-[40px] tracking-[1px] mx-auto">{`어떤 곳을 찾고 계세요?`}</div>
+          <div className="font-[700] h-[46px] text-[#f6f8f5] md:text-white text-[40px] tracking-[1px] mx-auto">{`어떤 곳을 찾고 계세요?`}</div>
         </div>
       </div>
-      <div className="z-10 flex justify-center w-full h-fit">
+      <div className="z-10 px-[16px] flex justify-center w-full h-fit">
         <div className="flex flex-col w-[574px] h-[196px]">
           <div className="w-full tracking-[1px] flex justify-center rounded-[6px] h-[40px] mb-[26px] bg-[#03142d85] text-white font-[500] items-center">
             법정동을 입력해 주세요
