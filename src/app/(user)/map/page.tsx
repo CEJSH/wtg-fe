730ce77c85ac915d2252537e5124867fa38f3cdd
CEJSH <eun2763@naver.com>
2024-08-16 12:44:48 +0900

    fix: refactor

--- src/app/(user)/map/page.tsx
+++ src/app/(user)/map/page.tsx
@@ -1,15 +1,13 @@
 'use client';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import NewSearchedArea from '../../../components/ResultSection';
 import { Suspense } from 'react';
 import GridSpinner from '@/components/ui/GridSpinner';
-
-import dynamic from 'next/dynamic';
+import ResultSection from '../../../components/ResultSection';
 const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
-      retry: 1, // 쿼리 실패 시 재시도 횟수
-      refetchOnWindowFocus: false, // 창이 포커스될 때 자동으로 데이터 리패치 방지
+      retry: 1,
+      refetchOnWindowFocus: false,
     },
   },
 });
@@ -17,9 +15,8 @@ const queryClient = new QueryClient({
 const MapPage = () => {
   return (
     <QueryClientProvider client={queryClient}>
-      {/* <SearchedArea /> */}
       <Suspense fallback={<GridSpinner />}>
-        <NewSearchedArea />
+        <ResultSection />
       </Suspense>
     </QueryClientProvider>
   );
