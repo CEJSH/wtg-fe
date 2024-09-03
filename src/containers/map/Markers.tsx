92834f92c904997df1934b852211342eaaaf9f28
CEJSH <eun2763@naver.com>
2024-09-04 05:06:40 +0900

    refactor: map markers

--- src/containers/map/Markers.tsx
+++ src/containers/map/Markers.tsx
@@ -9,7 +9,6 @@ interface MarkerProps {
 
 export default function Markers({ map, constData, setCurrentConst }: MarkerProps) {
   const { positions, fetchPositions } = useFetchPositions();
-
   const loadKakaoMarkers = () => {
     if (map && positions) {
       // 식당 데이터 마커 띄우기
@@ -22,24 +21,30 @@ export default function Markers({ map, constData, setCurrentConst }: MarkerProps
         // 마커를 생성합니다
         var marker = new window.kakao.maps.Marker({
           position: construction.latlng,
-          title: construction.title,
           image: markerImage,
-          clickable: true,
         });
         // 마커가 지도 위에 표시되도록 설정한다.
         marker.setMap(map);
 
-        // 마커 커서가 오버되었을 때 마커 위에 표시할 인포윈도우 생성
-        var content = `<div class="infowindow">${construction?.title}</div>`;
+        const placeName = construction.title.slice(7).replaceAll(' ', '');
+        const findUrl = `https://map.kakao.com/link/search/${placeName}`;
+        const bgColor = construction.cDay.startsWith('2024') ? `rgb(190, 18, 60)` : `rgb(249, 115, 22)`;
+        const content =
+          '<div class="customoverlay" aria-label="Open construction site details" style="padding-bottom: 1px; padding-left: 3px; padding-right: 3px; opacity: 0.7; line-height: 1.4; color: rgb(255, 255, 255); background-color: ' +
+          `${bgColor};` +
+          'font-size:10px; margin-right:8px; text-overflow: ellipsis;">' +
+          `  <a rel="preconnect" href=${findUrl} target="_blank">` +
+          `    <span class="title">${construction.title.slice(7)}</span>` +
+          '  </a>' +
+          '</div>';
 
-        //커스텀 오버레이를 생성합니다
-        var customOverlay = new window.kakao.maps.CustomOverlay({
+        const customOverlay = new window.kakao.maps.CustomOverlay({
+          map: map,
           position: construction.latlng,
           content: content,
           xAnchor: 0.6,
           yAnchor: 0.91,
         });
-
         // 마커에 마우스오버 이벤트 등록
         window.kakao.maps.event.addListener(marker, 'mouseover', function () {
           // 마커에 마우스오버 이벤트가 발생하면 인포윈도우를 마커위에 표시
